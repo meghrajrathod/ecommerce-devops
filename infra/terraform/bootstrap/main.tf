7ff7401d1efcbc89c7ee0d28c5c7f960f4fc02b4
Meghraj Rathod <25meghrajrathod@gmail.com>
2026-06-17 18:35:59 +0530

    feat: bootstrap terraform backend infrastructure

---
+++ infra/terraform/bootstrap/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "meghraj-terraform-state-375391317749"
+
+  tags = {
+    Project = "ecommerce-devops"
+    Owner   = "Meghraj"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Project = "ecommerce-devops"
+    Owner   = "Meghraj"
+  }
+}
